cb2b349d0677c1a91f10770489be39347eb64d94
HIDEKUNI Kajita <hide.nba@gmail.com>
2011-08-20 01:54:51 +0900

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UchikosiCom::Application.config.secret_token = '53c2c8e986d26668d808fc7033f508d5cb089370c30c2a1244b932835dfb1ca2ed901accbecb0fc2b9b6ad4952b7ae04c17d92a6c6023a9673d5812bed04e3af'
